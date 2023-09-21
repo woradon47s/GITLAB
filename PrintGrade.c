c8c9722f6008cf65d12081dfa7651caaf81933d0
wrxdS <pangpond.wrd@gmail.com>
2023-09-22 00:08:27 +0700

    Modified PrintGrade.c

--- PrintGrade.c
+++ PrintGrade.c
@@ -10,7 +10,7 @@ void main()
   printf("\nSTUDENT GPA LIST: \n"); 
   printf("ID----Name------------------------GPA\n"); 
   for (i=0;i<N; i++)
-    printf("%5d %-25s %5.2f\n", ID[i], Name[i], GPA[i]); 
+    printf("%5d %-25s %5.3f\n", ID[i], Name[i], GPA[i]); 
   printf("-------------------------------------\n");
   printf("Enter Student ID to Search (0 to End): "); 
   scanf("%d", &SearchID); 
